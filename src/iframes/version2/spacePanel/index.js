4193d02328c6fd412112d846656750e11d7a4162
Jacek Kolasa <jacek.kolasa@gmail.com>
2022-03-01 16:38:06 +0100

    prettify

--- src/iframes/version2/spacePanel/index.js
+++ src/iframes/version2/spacePanel/index.js
@@ -1,59 +1,50 @@
-import React from "react";
-import { useMount } from "react-use";
-import { Flex, H4 } from "@netdata/netdata-ui";
-import { useFocusDetector } from "hooks/use-focus-detector";
-import SpaceRooms from "./rooms";
-import ReplicatedNodes from "./nodes/replicatedNodes";
-import VisitedNodes from "./nodes/visitedNodes";
-import {
-  sendToParent,
-  sendToIframes,
-  useListenToPostMessage,
-} from "utils/post-message";
+import React from "react"
+import { useMount } from "react-use"
+import { Flex, H4 } from "@netdata/netdata-ui"
+import { useFocusDetector } from "hooks/use-focus-detector"
+import SpaceRooms from "./rooms"
+import ReplicatedNodes from "./nodes/replicatedNodes"
+import VisitedNodes from "./nodes/visitedNodes"
+import { sendToParent, sendToIframes, useListenToPostMessage } from "utils/post-message"
 
 const SpacePanel = () => {
-  useFocusDetector();
+  useFocusDetector()
 
   useMount(() => {
     sendToIframes({
       type: "hello-from-space-panel",
       payload: true,
-    });
+    })
     sendToParent({
       type: "hello-from-space-panel",
       payload: true,
-    });
-  });
+    })
+  })
 
-  const rooms = useListenToPostMessage("rooms");
-  const streamedHostsData = useListenToPostMessage("streamed-hosts-data");
-  const visitedNodes = useListenToPostMessage("visited-nodes");
-  const alarms = useListenToPostMessage("alarms") || [];
+  const rooms = useListenToPostMessage("rooms")
+  const streamedHostsData = useListenToPostMessage("streamed-hosts-data")
+  const visitedNodes = useListenToPostMessage("visited-nodes")
+  const alarms = useListenToPostMessage("alarms") || []
 
   const handleDeleteNode = (nodeId, url) => {
     sendToIframes({
       type: "delete-node-request",
       payload: { nodeId, url },
-    });
-  };
+    })
+  }
 
   return (
     <Flex flex overflow={{ vertical: "auto", horizontal: "hidden" }} column>
       {rooms && <H4>{rooms.spaceName}</H4>}
-      {rooms && !!rooms.results.length && (
-        <SpaceRooms rooms={rooms} alarms={alarms} />
-      )}
+      {rooms && !!rooms.results.length && <SpaceRooms rooms={rooms} alarms={alarms} />}
       {streamedHostsData && !!streamedHostsData.replicatedNodes.length && (
         <ReplicatedNodes replicatedNodes={streamedHostsData} />
       )}
       {visitedNodes && visitedNodes.length > 0 && (
-        <VisitedNodes
-          onDeleteClick={handleDeleteNode}
-          visitedNodes={visitedNodes}
-        />
+        <VisitedNodes onDeleteClick={handleDeleteNode} visitedNodes={visitedNodes} />
       )}
     </Flex>
-  );
-};
+  )
+}
 
-export default SpacePanel;
+export default SpacePanel
