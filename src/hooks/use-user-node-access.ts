abbb54473f441a359b65bee98b51b7904b92c96f
Jacek Kolasa <jacek.kolasa@gmail.com>
2022-03-08 12:07:38 +0100

    handle remaining cases + tests

--- src/hooks/use-user-node-access.ts
+++ src/hooks/use-user-node-access.ts
@@ -1,45 +1,61 @@
 import { cloudApiUrl } from "utils/constants"
 import { useHttp } from "./use-http"
-import { useCallback, useEffect, useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import { sendToParent } from "../utils/post-message"
 
-type NodeClaimedStatus = "NOT_CLAIMED" | "UNKNOWN" | "CLAIMED" | "NO_ACCESS"
+type NodeClaimedStatus = "NOT_CLAIMED" | "CLAIMED"
 type UserNodeAccess = "NO_ACCESS" | "ACCESS_OK"
-type UserPreference = "AGENT" | "CLOUD" | "UNDEFINED"
-type CTATYPE = "NAVIGATE" | "REFRESH"
-
-type UserStatus = "LOGGED_IN" | "LOGGED_OUT" | "UNKNOWN"
-type NodeLiveness = "LIVE" | "NOT_LIVE"
+type UserStatus = "LOGGED_IN" | "EXPIRED_LOGIN" | "UNKNOWN"
 
 // on 401
-type errorPayload = {
-  errorCode: "YDaYDPkXNV-696384"
+type ErrorUserUnknown = {
+  errorCode: string
   errorMsgKey: "ErrUnauthenticated"
   errorMessage: "no cookie"
 }
 
-type userAccessPayload = {
+export type UserAccessPayload = {
+  userStatus: "loggedIn" | "expiredLogin"
   userNodeStatus: "accessOK" | "noAccess"
   authorizedNodeIDs: string[]
 }
 
+export type AgentInfoPayload = {
+  claimed: boolean
+  reachable: boolean
+}
+
 export type AgentMessagePayload = {
   userStatus: UserStatus
+  nodeClaimedStatus: NodeClaimedStatus
+  userNodeAccess: UserNodeAccess
 }
 
 export const useUserNodeAccess = ({ machineGUID }: { machineGUID: null | string }) => {
-  const [userAccess, , , userAccessError] = useHttp(
+  const [userAccess, , , userAccessError] = useHttp<UserAccessPayload>(
     `${cloudApiUrl}agents/${machineGUID}/user_access`,
     Boolean(machineGUID)
   )
-  const [agentInfo, , , agentInfoError] = useHttp(
+  const [agentInfo, , , agentInfoError] = useHttp<AgentInfoPayload>(
     `${cloudApiUrl}agents/${machineGUID}/info`,
     Boolean(machineGUID)
   )
   const message2Agent = useMemo(() => {
     if ((!userAccess && !userAccessError) || !agentInfo) return
+
+    let userStatus: UserStatus
+    if (userAccess?.userStatus === "loggedIn") userStatus = "LOGGED_IN"
+    else if (userAccess?.userStatus === "expiredLogin") userStatus = "EXPIRED_LOGIN"
+    else userStatus = "UNKNOWN"
+
+    const nodeClaimedStatus: NodeClaimedStatus = agentInfo.claimed ? "CLAIMED" : "NOT_CLAIMED"
+    const userNodeAccess: UserNodeAccess =
+      userAccess?.userNodeStatus === "accessOK" ? "ACCESS_OK" : "NO_ACCESS"
+
     return {
-      userStatus: "UNKNOWN",
+      userStatus,
+      nodeClaimedStatus,
+      userNodeAccess,
     }
   }, [userAccess, userAccessError, agentInfo])
   useEffect(() => {
